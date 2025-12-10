1eab87fffe50acebdcddd59d83b15b0eb1266347
Ana Ferreira <anaferconsultoria@gmail.com>
2025-12-10 19:08:06 -0300

    add aula18

---
+++ Exercicios/aula18/funcao.js
@@ -0,0 +1,13 @@
+function parimpar(n) {
+    if (n % 2 == 0) {
+        return 'par'
+    } else {
+        return 'ímpar'
+    }
+}
+
+let res = parimpar(4)
+         
+
+console.log(res)
+console.log(parimpar(5))    
