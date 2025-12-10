b050892e002ef1846e81457e24ac1f7a953816fd
Ana Ferreira <anaferconsultoria@gmail.com>
2025-12-10 18:17:01 -0300

    add

---
+++ Exercicios/aula15/ambiente.js
@@ -0,0 +1,9 @@
+/*var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+for (var c = 1; c <= 6; c++) {
+    console.log(`Passo ${c}`)
+}
---
+++ Exercicios/aula17/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+
+console.log(`Nosso vetor tem ${num.length} elementos`)
+console.log(`O primeiro valor do vetor é ${num[1]}`)
+
+let pos = num.indexOf(-1)
+if(pos == -1){
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
